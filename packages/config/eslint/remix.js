6f87565c7fb20df653284dd9549c847dfddfecf7
Cillian Barron <barroncillian@gmail.com>
2023-08-01 22:22:20 +0100

    feat: set up linting

---
+++ packages/config/eslint/remix.js
@@ -0,0 +1,8 @@
+/** @type {import('eslint').Linter.Config} */
+const config = {
+    extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+    rules: {},
+  };
+  
+  module.exports = config;
+  
