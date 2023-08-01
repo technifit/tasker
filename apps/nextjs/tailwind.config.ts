4245fc6521887954bfce1f911144bea1859c6f8e
Cillian Barron <barroncillian@gmail.com>
2023-08-01 21:01:50 +0100

    chore: :fire: remove apps

--- apps/nextjs/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import baseConfig from "@technifit/tailwind-config";
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [...baseConfig.content, "../../packages/ui/src/**/*.{ts,tsx}"],
-  presets: [baseConfig],
-} satisfies Config;
