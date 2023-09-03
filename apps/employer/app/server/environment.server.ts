a7598b9c0da84086bc92ad616ac9a9f845615ee1
Cillian Barron <barroncillian@gmail.com>
2023-09-03 17:17:06 +0100

    chore: :coffin: remove dead code

--- apps/employer/app/server/environment.server.ts
+++ apps/employer/app/server/environment.server.ts
@@ -19,19 +19,3 @@ const getPublicKeys = () => {
 };
 
 export { environment, getPublicKeys };
-
-// try {
-//   environmentSchema.parse(process.env);
-// } catch (err) {
-//   if (err instanceof z.ZodError) {
-//     const { fieldErrors } = err.flatten();
-//     const errorMessage = Object.entries(fieldErrors)
-//       .map(([field, errors]) =>
-//         errors ? `${field}: ${errors.join(', ')}` : field,
-//       )
-//       .join('\n  ');
-//     throw new Error(`Missing environment variables:\n  ${errorMessage}`);
-
-//     process.exit(1);
-//   }
-// }
