bce1a0d85969aa525600f7494c6bd5d20dccd7d8
Cillian Barron <barroncillian@gmail.com>
2024-01-10 21:49:36 +0000

    fix: remove deprecated type

--- apps/employer/app/routes/_auth/_layout.server.ts
+++ apps/employer/app/routes/_auth/_layout.server.ts
@@ -1,8 +1,8 @@
-import type { DataFunctionArgs } from '@vercel/remix';
+import type { LoaderFunctionArgs } from '@vercel/remix';
 
 import { requireUnauthenticatedUser } from '~/lib/guards/auth-guard.server';
 
-export const loader = async (args: DataFunctionArgs) => {
+export const loader = async (args: LoaderFunctionArgs) => {
   await requireUnauthenticatedUser(args);
 
   return null;
