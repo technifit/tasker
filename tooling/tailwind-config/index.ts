3346b42c7ace1b658805116cdf76e4f0de69c4d5
Cillian Barron <barroncillian@gmail.com>
2024-03-11 23:02:32 +0000

    feat: add new shadcn components

--- tooling/tailwind-config/index.ts
+++ tooling/tailwind-config/index.ts
@@ -95,12 +95,17 @@ export default {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'caret-blink': {
+          '0%,70%,100%': { opacity: '1' },
+          '20%,50%': { opacity: '0' },
+        },
       },
       animation: {
         'fade-up': 'fade-up 0.5s',
         'fade-down': 'fade-down 0.5s',
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'caret-blink': 'caret-blink 1.25s ease-out infinite',
       },
     },
   },
