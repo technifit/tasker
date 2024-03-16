94464d06f0599640aae615b1b626d16ef49378ac
Cillian Barron <barroncillian@gmail.com>
2024-03-16 08:14:35 +0000

    feat: fix auth layout

--- apps/employer/app/routes/_auth/_layout.tsx
+++ apps/employer/app/routes/_auth/_layout.tsx
@@ -1,8 +1,5 @@
-import { Link, Outlet } from '@remix-run/react';
+import { Outlet } from '@remix-run/react';
 import type { LoaderFunctionArgs } from '@vercel/remix';
-import { $path } from 'remix-routes';
-
-import { Typography } from '@technifit/ui';
 
 import { requireUnauthenticatedUser } from '~/lib/guards/auth-guard.server';
 
@@ -14,25 +11,10 @@ export const loader = async (args: LoaderFunctionArgs) => {
 
 const Layout = () => {
   return (
-    <div className='container grid grid-cols-1 py-4 lg:grid-cols-2'>
-      <div className='hidden lg:flex lg:flex-col lg:justify-between lg:gap-2'>
-        <div className='flex justify-start'>
-          <Link prefetch='intent' to={$path('/')} className='inline-flex items-center gap-1'>
-            Tasker
-          </Link>
-        </div>
-        <Typography variant={'blockquote'} className='mb-4'>
-          <Typography className='text-lg'>
-            &ldquo;This system has saved me countless hours of work and helped me complete jobs for my clients faster
-            than ever before.&rdquo;
-          </Typography>
-          <footer className='text-sm'>Joe Bloggs</footer>
-        </Typography>
-      </div>
-      <div className='flex flex-col gap-2'>
-        <div className='flex grow flex-col items-center justify-center'>
-          <Outlet />
-        </div>
+    <div className='container flex flex-col md:h-screen md:flex-row-reverse'>
+      <div className='mx-auto flex w-full items-start md:w-1/3 md:items-center'></div>
+      <div className='flex grow justify-center md:w-2/3 md:border-r'>
+        <Outlet />
       </div>
     </div>
   );
