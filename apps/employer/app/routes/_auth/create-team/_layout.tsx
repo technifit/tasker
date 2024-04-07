96f29bf30f7ff26da143b64ae6fdbad6e2850fef
Cillian Barron <barroncillian@gmail.com>
2024-04-07 19:59:04 +0100

    feat: add stepper to layout

--- apps/employer/app/routes/_auth/create-team/_layout.tsx
+++ apps/employer/app/routes/_auth/create-team/_layout.tsx
@@ -1,12 +1,25 @@
 import { Outlet } from '@remix-run/react';
 
-import { Card } from '@technifit/ui';
+import { Card, Progress } from '@technifit/ui';
+
+import { useGetStepProgress } from './config';
 
 export const CreateTeamLayout = () => {
+  const { activeStep, totalSteps } = useGetStepProgress();
+
   return (
-    <Card className='w-full sm:w-1/2 md:max-w-xl lg:w-1/3'>
-      <Outlet />
-    </Card>
+    <div className='flex w-full flex-col items-center gap-6 align-middle'>
+      <Card className='w-full sm:w-1/2 md:max-w-xl lg:w-1/3'>
+        <Outlet />
+      </Card>
+      {totalSteps > 1 ? (
+        <div className='flex w-full justify-between gap-2 sm:w-1/6 md:max-w-lg lg:w-1/4'>
+          {Array.from({ length: totalSteps }, (_item, index) => (
+            <Progress size={'sm'} key={index} value={index <= activeStep - 1 ? 100 : 0} />
+          ))}
+        </div>
+      ) : null}
+    </div>
   );
 };
 
