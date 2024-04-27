4c1e9df85916d58cbdfd553139ae18eeb19123e3
Cillian Barron <barroncillian@gmail.com>
2024-04-27 21:42:04 +0100

    feat: update packages & remove employer app

--- apps/employer/app/routes/resources+/theme/_index.tsx
+++
@@ -1,7 +0,0 @@
-import { createThemeAction } from 'remix-themes';
-
-import { themeSessionResolver } from './theme.server';
-
-export const THEME_ROUTE_PATH = '/resources/theme';
-
-export const action = createThemeAction(themeSessionResolver);
--- apps/employer/app/ui/tailwind-indicator.tsx
+++
@@ -1,9 +0,0 @@
-import { ScreenSizeIndicator } from '@technifit/ui';
-
-import { getPublicEnv } from './public-env';
-
-export function TailwindIndicator() {
-  if (getPublicEnv('NODE_ENV') === 'production' || getPublicEnv('VERCEL_ENV') === 'production') return null;
-
-  return <ScreenSizeIndicator />;
-}
