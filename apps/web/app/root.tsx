a6bd33fd75f243bc2460f5a458cd68b14f4a4d1b
Cillian Barron <barroncillian@gmail.com>
2024-06-09 15:28:43 +0100

    feat: add session middleware & idempotency test (#625)

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -1,5 +1,6 @@
 import { ClerkApp } from '@clerk/remix';
 import { rootAuthLoader } from '@clerk/remix/ssr.server';
+import { createCookieSessionStorage } from '@remix-run/node';
 import type { LinksFunction, LoaderFunctionArgs } from '@remix-run/node';
 import {
   isRouteErrorResponse,
@@ -13,7 +14,11 @@ import {
 } from '@remix-run/react';
 import { captureRemixErrorBoundaryError, withSentry } from '@sentry/remix';
 import { publicEnvSchema } from 'server/env';
+import { serverOnly$ } from 'vite-env-only/macros';
 
+import { idempotency } from '@technifit/middleware/idempotency';
+import { createSessionMiddleware } from '@technifit/middleware/session';
+import type { SessionData, SessionFlashData } from '@technifit/middleware/session';
 import { cn } from '@technifit/ui/utils';
 
 import { PublicEnvironment } from './lib/environment/public-env';
@@ -23,6 +28,22 @@ const interWoff = Array.from({ length: 9 }, (_, i) => `inter/inter-latin-ext-${i
 const interWoff2 = Array.from({ length: 9 }, (_, i) => `inter/inter-latin-ext-${i * 100 + 100}-normal.woff2`);
 
 const fonts = [...interWoff, ...interWoff2];
+
+const session = createSessionMiddleware(
+  createCookieSessionStorage<SessionData, SessionFlashData>({
+    cookie: {
+      name: '__session_test_cb',
+      path: '/',
+      sameSite: 'lax',
+      secrets: ['s3cret1'],
+    },
+  }),
+);
+
+// export your middleware as array of functions that Remix will call
+// wrap middleware in serverOnly$ to prevent it from being bundled in the browser
+// since remix doesn't know about middleware yet
+export const middleware = serverOnly$([session, idempotency]);
 
 export const links: LinksFunction = () => [
   { rel: 'stylesheet', href: styles },
