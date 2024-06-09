3d4392a96c82400f77644a3f6c61ea932b107838
Cillian Barron <barroncillian@gmail.com>
2024-06-09 19:54:15 +0100

    fix: comment unused middleware

--- packages/middleware/src/access-token.ts
+++ packages/middleware/src/access-token.ts
@@ -1,27 +1,27 @@
-import type { MiddlewareFunctionArgs } from 'remix-create-express-app/middleware';
+// import type { MiddlewareFunctionArgs } from 'remix-create-express-app/middleware';
 
-import { SessionContext } from './session';
+// import { SessionContext } from './session';
 
-/**
- * Middleware that handles the access token.
- * It retrieves the access token from the request URL's query parameters or from the session.
- * If the access token is not found, it throws an error.
- */
-const accessToken = ({ context, next, request }: MiddlewareFunctionArgs) => {
-  const sessionContext = context.get(SessionContext);
+// /**
+//  * Middleware that handles the access token.
+//  * It retrieves the access token from the request URL's query parameters or from the session.
+//  * If the access token is not found, it throws an error.
+//  */
+// const accessToken = ({ context, next, request }: MiddlewareFunctionArgs) => {
+//   const sessionContext = context.get(SessionContext);
 
-  const accessToken = new URL(request.url).searchParams.get('access_token');
-  const existingToken = sessionContext.get('access_token');
+//   const accessToken = new URL(request.url).searchParams.get('access_token');
+//   const existingToken = sessionContext.get('access_token');
 
-  if (accessToken) {
-    sessionContext.set('access_token', accessToken);
-  }
+//   if (accessToken) {
+//     sessionContext.set('access_token', accessToken);
+//   }
 
-  if (!accessToken && !existingToken) {
-    throw Error('Access token is required');
-  }
+//   if (!accessToken && !existingToken) {
+//     throw Error('Access token is required');
+//   }
 
-  return next();
-};
+//   return next();
+// };
 
-export { accessToken };
+// export { accessToken };
