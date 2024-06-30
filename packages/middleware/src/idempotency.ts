aec74ad54a03fda60c42e6b5193aff989c491822
Cillian Barron <barroncillian@gmail.com>
2024-06-30 13:17:38 +0100

    feat: add withAuthentication middleware (#740)

--- packages/middleware/src/idempotency.ts
+++ packages/middleware/src/idempotency.ts
@@ -2,16 +2,12 @@ import type { MiddlewareFunctionArgs } from 'remix-create-express-app/middleware
 
 import { newId } from '@technifit/id/new-id';
 
-import { SessionContext } from './session';
-
 /**
  * Middleware function that adds idempotency key to the session.
  * The idempotency key is used to ensure that a request with the same key is processed only once.
  */
-async function idempotency({ context, next }: MiddlewareFunctionArgs) {
-  const sessionContext = context.get(SessionContext);
-
-  sessionContext.set('idempotency_key', newId('idempotency'));
+async function idempotency({ next }: MiddlewareFunctionArgs) {
+  console.log('idempotency', newId('idempotency'));
 
   // return the response
   return next();
